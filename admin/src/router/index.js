bc9753996e47cee27345ca28855e2a480f4ab54a
Daisy-zhai <1162445111@qq.com>
2020-12-07 23:56:56 +0800

    2 day

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
 import Main from '../views/Main.vue'
+import CategoryEdit from '../views/CategoryEdit.vue'
 
 Vue.use(VueRouter)
 
@@ -9,7 +10,10 @@ const routes = [
   {
     path: '/',
     name: 'Main',
-    component: Main
+    component: Main,
+    children:[
+      {path:'/categories/create', component:CategoryEdit}
+    ]
   },
   // {
   //   path: '/about',
