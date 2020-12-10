ed3c972d8ba953c170bb126e6c2236039364e369
Daisy-zhai <1162445111@qq.com>
2020-12-11 02:19:03 +0800

    day5

--- admin/src/views/ItemEdit.vue
+++ admin/src/views/ItemEdit.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="about">
-    <h1>{{id ? '编辑' : '新建'}}分类</h1>
+    <h1>{{id ? '编辑' : '新建'}}物品</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
-      <el-form-item label="上级分类">
-        <el-select v-model="model.parent">
-          <el-option v-for="item in parents" :key="item._id"
-          :label="item.name" :value="item._id"></el-option>
-        </el-select>
-      </el-form-item>
+      
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item label="图标">
+        <el-input v-model="model.icon"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -26,18 +24,17 @@ export default {
   data(){
     return{
       model:{},
-      parents:[],
     }
   },
   methods:{
     async save(){
       let res
       if (this.id){
-        res = await this.$http.put(`rest/categories/${this.id}`,this.model)
+        res = await this.$http.put(`rest/items/${this.id}`,this.model)
       } else {
-        res = await this.$http.post('rest/categories/rest/',this.model)
+        res = await this.$http.post('rest/items/',this.model)
       }
-      this.$router.push('/categories/list')
+      this.$router.push('/items/list')
       this.$message({
         type:'success',
         message : '保存成功'
@@ -45,16 +42,11 @@ export default {
       console.log(res)
     },
     async fetch(){
-      const res = await this.$http.get(`rest/categories/${this.id}`)
+      const res = await this.$http.get(`rest/items/${this.id}`)
       this.model = res.data
-    },
-    async fetchParents(){
-      const res = await this.$http.get(`rest/categories`)
-      this.parents = res.data
     },
   },
   created(){
-    this.fetchParents()
     this.id && this.fetch()
   }
 
